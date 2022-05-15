4159197540100822498955b7eb5bdfb6caba0334
Christopher Bo <csb3946@g.rit.edu>
2022-05-15 11:44:06 -0400

    removed videoPath bc we can still use sysargs with bash script, changed startTime and endTime to be lists of minutes, seconds, and frames for easier parsing

--- fps_detector.py
+++ fps_detector.py
@@ -22,26 +22,34 @@ else:
 # Read two frames, last and current, and convert current to gray.
 ret, last_frame = video_capture.read()
 ret, current_frame = video_capture.read()
-gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY) #may want to remove this
 
 min = 0
 sec = 0
 frame = -1
 
-startTime = ""
-endTime = ""
-videoPath = ""
+startTime = []
+endTime = []
 
 #read rgb.txt and take out anything from it
 with open(DEST, "r") as file:
+    #csv reader makes each line a list of strings + strips line of extra whitespaces/line returns
     csv_reader = csv.reader(file)
     i = 0
-    for line in csv_reader and i == 0: #only read first line
-        i+=1
-        startTime = line[0]
-        endTime = line[1]
-        videoPath = line[2]
-        #print(line)
+    for line in csv_reader: 
+        if i == 0 and len(line) == 3: #only read first line
+            i += 1
+            print(line)
+            startTokens = line[0].split(":") #[hours, minutes, seconds, frames]
+            startTime = [int(startTokens[1]), int(startTokens[2]), int(startTokens[3])]
+            endTokens = line[1].split(":") #[hours, minutes, seconds, frames]
+            endTime = [int(endTokens[1]), int(endTokens[2]), int(endTokens[3])]
+        else:
+            break
+    
+if(endTime == []): #couldnt read file
+    startTime = [0, 0, 0]
+    endTime = [0, 0, 0]
 
 
 #clear rgb.txt for writing
@@ -54,10 +62,6 @@ while True:
 
     if not ret:
         break
-
-    gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
-
-    diff = cv2.absdiff(last_frame, current_frame)
     
     #string conversions- if frame/sec/min 9 or less add a prefix 0 for consistency
     sframe = str(frame)
@@ -72,14 +76,22 @@ while True:
     if min < 10:
         smin = "0" + str(min)
         
-    if np.mean(diff) > 0.7 and frame != -1:
-        print("Motion of: " + str(round(np.mean(diff), 2)) + " detected at frame " + smin + ":" + ssec + ":" + sframe)
-        with open(DEST, "a") as file:
-            file.write(str(round(np.mean(diff), 2)) + "," + smin + ":" + ssec + ":" + sframe + "\n")
+    currentTime = smin + ":" + ssec + ":" + sframe
+        
+    if(currentTime >= startTime and currentTime <= endTime): 
+        #image processing
+        gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY) #may want to remove this
+        diff = cv2.absdiff(last_frame, current_frame)
+            
+        #write if last frame is different enough from current frame
+        if np.mean(diff) > 0.7 and frame != -1:
+            print("Motion of: " + str(round(np.mean(diff), 2)) + " detected at frame " + smin + ":" + ssec + ":" + sframe)
+            with open(DEST, "a") as file:
+                file.write(str(round(np.mean(diff), 2)) + "," + currentTime + "\n")
 
     #uncomment to show difference matte
     #cv2.imshow("Motion detected", diff)
-    time.sleep(0.1)
+    #time.sleep(0.1)
     
     #frame/sec/min counter
     frame += 1
