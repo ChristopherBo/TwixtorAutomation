26249a2f9ed106eac22de70e54a55482a97ab460
Christopher Bo <csb3946@g.rit.edu>
2022-05-18 19:54:37 -0400

    updated example batch script

--- fps_analyzer.bat
+++ fps_analyzer.bat
@@ -1,15 +1,8 @@
 @echo off
 echo ///////////////////////////////
-echo Installing required packages...
-echo ///////////////////////////////
-pip install scipy
-pip install numpy
-pip install pywavelets
-pip install matplotlib
-echo ///////////////////////////////
 echo Detecting fps...
 echo ///////////////////////////////
-python "G:\Recording Footage\Tutorials\TwixtorAutomation\fps_detector.py" "G:\Recording Footage\Tutorials\TwixtorAutomation\Violet Evergarden, Episode 1.mp4"
+"G:\Recording Footage\Tutorials\TwixtorAutomation\fps_detector.exe" "G:\Recording Footage\Tutorials\TwixtorAutomation\Violet Evergarden, Episode 1.mp4"
 echo Finished! This program will close in 5 seconds. You can also close it with Ctrl + C.
 timeout 6
 undefined
